f1970e89ac29ee3ae99a2e9a0093ad344c31205e
YizYah <yyakovson613@gmail.com>
2021-04-30 14:46:39 -0500

    feat: creation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 /* ns__file unit: standard, comp: src/index.ts */
 /* ns__custom_start beginning */
+import {interactic} from './custom/interactic'
+/* ns__custom_end export */
 /* ns__custom_end beginning */
 
 /* types */
 
 /* ns__custom_start export */
 // replace this with your own export
-module.exports = "hello world"
-/* ns__custom_end export */
+module.exports = interactic
